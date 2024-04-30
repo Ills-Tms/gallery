9b3701b89a4d9fa6553e0fc48384100e54662958
itd <itd>
2024-04-30 11:48:35 +0200

    acsa

---
+++ script.js
@@ -0,0 +1,15 @@
+const container = document.querySelector("#container");
+const bigImgDiv = document.querySelector("#bigImgDiv");
+const targetImg = document.querySelector("#targetImg");
+
+const kepek = ["kutya.webp", "macska.jpg", "delfin.png"];
+
+for (const kep of kepek) {
+  img = document.createElement("img");
+  img.src = "img/" + kep;
+  img.width = "200";
+  container.appendChild(img);
+  img.addEventListener("click", function () {
+    targetImg.src = this.src;
+  });
+}
