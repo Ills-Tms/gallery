2523c7ff40f96ba1abdf45a3e9767172345f03dc
itd <itd>
2024-05-15 09:15:15 +0200

    blflblf

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const bigImgDiv = document.querySelector("#bigImgDiv");
 const targetImg = document.querySelector("#targetImg");
 
 const kepek = ["kutya.webp", "macska.jpg", "delfin.png"];
+bigImgDiv.classList.add('hidden')
 
 for (const kep of kepek) {
   img = document.createElement("img");
@@ -10,11 +11,11 @@ for (const kep of kepek) {
   img.width = "200";
   container.appendChild(img);
   img.addEventListener("click", function () {
-    targetImg.classList.remove('hidden')
+    bigImgDiv.classList.remove('hidden')
     targetImg.src = this.src;
   });
 }
 
-targetImg.addEventListener('click',()=>{
-  targetImg.classList.add('hidden')
+bigImgDiv.addEventListener('click',function(){
+  bigImgDiv.classList.add('hidden')
 })
